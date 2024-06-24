c60001ea20b7ec3afac2c20e66c28429b0643155
geekynice <nicebanjaraa@gmail.com>
2024-06-24 14:39:57 -0400

    preferences added

--- NCT_HACKS_APP/urls.py
+++ NCT_HACKS_APP/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('preferences/', views.preferences_view, name='preferences'),
 ]
--- NCT_HACKS_APP/views.py
+++ NCT_HACKS_APP/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from .models import Preferences, UserModel
+from django.contrib.auth.decorators import login_required
 from django.core.files.storage import FileSystemStorage
 def index(request):
     return render(request, 'index.html')
@@ -36,7 +37,7 @@ def signup_view(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('index')  
+            return redirect('preferences')  
 
     return render(request, 'signup.html')
 
@@ -53,6 +54,29 @@ def login_view(request):
             messages.error(request, 'Invalid credentials')
 
     return render(request, 'login.html')
+
+@login_required
+def preferences_view(request):
+    if request.method == 'POST':
+        diet_option = request.POST.get('diet_option')
+        nut_allergic = request.POST.get('nut_allergic') == 'on'
+        goal = request.POST.get('goal')
+
+        # Ensure the logged-in user has a CustomUser instance
+        custom_user = UserModel.objects.get(user=request.user)
+
+        # Save preferences for the user
+        Preferences.objects.create(
+            user=custom_user,
+            diet_option=diet_option,
+            nut_allergic=nut_allergic,
+            goal=goal
+        )
+
+        return redirect('index')  # Redirect to a home page or another appropriate page
+
+    return render(request, 'preferences.html')
+
 
 def logout_view(request):
     logout(request)
